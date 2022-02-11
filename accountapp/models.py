bb7107d18647df0d8eb524f04900eb1817aa511e
d0422 <rlfehd2013@naver.com>
2022-02-11 20:26:28 +0900

    just commit

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+# db로 넘겨주기 (helloworld라는 모델을 만들어서 넘겨줌)
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from accountapp.models import HelloWorld
+
+
 def hello_world(request):
-    return render(request,'accountapp/hello_world.html')
+    if request.method =="POST":
+        #request가 POST방식일 경우에 hello_world_input에 있는 내용을 tmp로 가져와라
+        tmp=request.POST.get("hello_word_input")
+        #HelloWorld모델 변수를 만들어서 text에 tmp를 넣어줌
+        new_hello_world=HelloWorld()
+        new_hello_world.text=tmp
+        #db에 저장
+        new_hello_world.save()
+
+        hello_world_list=HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request,'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
